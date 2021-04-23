7d7b14e727c7fa19b6b554a4671855352848976a
Sim Jao <jao.sim@gmail.com>
2021-04-24 02:42:16 +0800

    * first official changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import './App.css';
+import SocialMediaLinks from "./components/SocialMediaLinks/SocialMediaLinks";
+import './App.scss';
 
 function App() {
-   return (
-      <div className="App">
-         <div className="BackgroundImage"/>
-      </div>
-   );
+	return (
+		<div className="App">
+			<div className="header">
+				<div className="logo"><strong>Sim</strong> Jao</div>
+				<ul>
+					<li className="about-me">About Me</li>
+					<li className="portfolio">Portfolio</li>
+					<li className="blogs">Blogs</li>
+				</ul>
+			</div>
+			<div className="content">
+				<div className="profile-image" />
+				<div className="profile-description">
+					<div className="greetings bybyn">ᜃᜋᜓᜐ᜔ᜆ</div>
+					<h1 className="name">Sim Jao</h1>
+					<p>
+						I am a web designer and developer specializing in JavasSript including but not limited to React JS.
+					</p>
+					
+					<p>
+						My experiences made me grow a lot working from an average joe to a multinational. With Javascript, I
+						am a fullstack developer.
+					</p>
+					<p>
+						At times, I maybe vocal and loud, but I am a more laid-back person.
+					</p>
+				</div>
+			</div>
+			<div className="footer">
+				<span><strong>What</strong> I Do</span>
+				<SocialMediaLinks />
+			</div>
+		</div>
+	);
 }
 
 export default App;
