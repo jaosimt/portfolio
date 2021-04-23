e9b1573483b3fffb6c9e42651be2395b740bf248
Sim Jao <jao.sim@gmail.com>
2021-04-24 02:58:54 +0800

    * profile header profile name

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
 	return (
 		<div className="App">
 			<div className="header">
-				<div className="logo"><strong>Sim</strong> Jao</div>
+				<div className="logo bybyn"><strong>ᜐᜒᜋ᜔</strong> ᜑᜏ᜔</div>
 				<ul>
 					<li className="about-me">About Me</li>
 					<li className="portfolio">Portfolio</li>
