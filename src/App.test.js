3a50f6efc062461b94fad565e660a994b55a7c2b
Sim Jao <jao.sim@gmail.com>
2021-04-24 05:10:18 +0800

    * fix test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders portfolio: sim jao', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/sim jao/i);
   expect(linkElement).toBeInTheDocument();
 });
