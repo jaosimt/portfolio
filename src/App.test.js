6fe453431d4bdef5777f4636ccfbc69df1549edf
Sim Jao <jao.sim@gmail.com>
2021-04-30 22:00:31 +0800

    * renames

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders portfolio: sim jao', () => {
+test('renders Portfolio: Sim Tero Jao', () => {
   render(<App />);
   const linkElement = screen.getByText(/sim jao/i);
   expect(linkElement).toBeInTheDocument();
