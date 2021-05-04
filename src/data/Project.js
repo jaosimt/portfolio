a3f98aff86c4f0be3685f982380df5c4711a3fdc
Sim Jao <jao.sim@gmail.com>
2021-05-04 14:36:36 +0800

    * added project simo-ui

--- src/data/Project.js
+++ src/data/Project.js
@@ -1,9 +1,11 @@
 // PROJECT LOGOS
 import baybayinPilipinas from './images/projects/baybayin-pilipinas.jpg';
 import underConstruction from '../components/PageNotFound/under-construction.png';
+import simoUI from './images/projects/simo-ui.jpg';
 
 import {
-	baybayinPilipinasDetails
+	baybayinPilipinasDetails,
+	simoUIDetails
 } from './ProjectDetails';
 
 export const projectList = [
@@ -16,6 +18,15 @@ export const projectList = [
 		title: 'Online Baybayin Keyboard & Translator',
 		image: baybayinPilipinas,
 		details: baybayinPilipinasDetails
+	}, {
+		id: 'simo-ui',
+		subject: {
+			name: 'simo UI Widget',
+			website: '//simo-ui.jaosimt.com/'
+		},
+		title: 'JS Library',
+		image: simoUI,
+		details: simoUIDetails
 	}, {
 		id: 'more-to-come',
 		subject: {
--- src/data/ProjectDetails.js
+++ src/data/ProjectDetails.js
@@ -1,6 +1,24 @@
 import React, { Fragment } from "react";
 import philippineScripts from './images/projects/philippineScripts.jpg';
 import baybayinCharacters from './images/projects/baybayinCharacters.png';
+import sImoUI from './images/projects/simo-ui-full.jpg';
+
+export const simoUIDetails = <Fragment>
+	<h5 className="topic-label"><a href="//simo-ui.jaosimt.com">simo-ui.jaosimt.com</a></h5>
+	<p>
+		A lightweight Javascript library.  Originally built just for fun and practice.  Turns out it came handy on one of my small web project.  Thus, it can be a tool or a stepping stone for students and beginners alike.
+	</p>
+	<p>The goal for the library is to automatically load css dependency for the inclusive ui components and unload them whenever no longer present on the screen.  An automatic process that does not require intervention from the user.</p>
+	<p>Can be for coding practice or even use in an HTML, CSS & Vanilla JS web project.  I have successfully use this in one of my web projects in the past which details will also be included in this very same project list.</p>
+	<p>Download simo-ui (sImoJS) library <a href="//simo-ui.jaosimt.com/sImoJS.zip">here</a>.</p>
+	<p>Copy it in your web project and simply paste the following line at the end of your html's body tag.</p>
+	<code>{`<script type="text/javascript" src="sImoJS/sImo.js"></script>`}</code>
+	<p>For more information and documentation, click the logo or the link provided above.</p>
+	<a href="//simo-ui.jaosimt.com">
+		<div className="inline-image" style={ { backgroundSize: 'contain', backgroundImage: `url(${ sImoUI })` } } />
+	</a>
+	<br/>
+</Fragment>
 
 export const baybayinPilipinasDetails = <Fragment>
 	<h5 className="topic-label"><a href="//baybayin.jaosimt.com" target="_blank" rel="noreferrer">baybayin.jaosimt.com</a></h5>
